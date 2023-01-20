dfc31357b27fadc00af8afe7654be756cd97bc9e
Marco Klaassen <mklaasse@redhat.com>
2023-01-20 15:08:31 +0100

    remove unused annotation

--- src/main/java/click/klaassen/feedback/Feedback.java
+++ src/main/java/click/klaassen/feedback/Feedback.java
@@ -3,12 +3,10 @@ package click.klaassen.feedback;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@ToString
 public class Feedback {
     public Integer rating;
 }
